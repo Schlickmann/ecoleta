50b86da74eebba26bbeed55c8635d99f27e196ea
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-05 15:22:45 -0700

    created migrations

--- backend/src/database/migrations/002_create_items.ts
+++ backend/src/database/migrations/002_create_items.ts
@@ -1,2 +1,13 @@
-export async function up() {}
-export async function down() {}
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("items", (table) => {
+    table.increments("id").primary();
+    table.string("image").notNullable();
+    table.string("title").notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("items");
+}
--- backend/src/database/migrations/003_create_point_items.ts
+++ backend/src/database/migrations/003_create_point_items.ts
@@ -1,2 +1,13 @@
-export async function up() {}
-export async function down() {}
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("point_items", (table) => {
+    table.increments("id").primary();
+    table.integer("point_id").notNullable().references("id").inTable("points");
+    table.integer("item_id").notNullable().references("id").inTable("items");
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("point_items");
+}

cbf6b28e18651772f612e86a0ba811b4172bc5ae
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-05 16:31:35 -0700

    created controller to handle point of collect methods

---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,3 @@
+class ItemsController {}
+
+export default ItemsController;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import knex from "./database/connection";
+import PointsController from "./controllers/PointsController";
 
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.get("/items", async (request, response) => {
   const items = await knex("items").select("*");
@@ -15,41 +17,6 @@ routes.get("/items", async (request, response) => {
   return response.json(serializedItems);
 });
 
-routes.post("/points", async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => ({
-    item_id,
-    point_id,
-  }));
-
-  await trx("point_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.post("/points", pointsController.create);
 
 export default routes;

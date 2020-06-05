ba9295f54e50c39980742a6ce615149513a867d2
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-05 15:45:09 -0700

    added static route to serve images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
+import path from "path";
 import express from "express";
 import routes from "./routes";
 
 const app = express();
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);

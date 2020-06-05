b2082c58e8d021e7d00fbbb7c4dafb51fa687510
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-05 15:52:02 -0700

    listing items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => response.send("Hello World!!"));
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => ({
+    name: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return response.json(serializedItems);
+});
 
 export default routes;

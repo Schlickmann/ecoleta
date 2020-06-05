41abfa8937f293ba2716718ee93e4b43a266cabe
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-06-05 16:23:08 -0700

    added functionality to add a point of collect

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import routes from "./routes";
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
